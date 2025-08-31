83a5a02bcd246c7c86e0ceca202a2a4cb3424573
pjames <pjames@gmail.com>
2025-09-01 06:18:55 +0700

    Send data with encryption

--- onix-api/Utils/EncryptionUtils.cs
+++ onix-api/Utils/EncryptionUtils.cs
@@ -25,5 +25,26 @@ namespace Its.Onix.Api.Utils
                 }
             }
         }
+
+        public static string Decrypt(string cipherTextBase64, string key, string iv)
+        {
+            byte[] cipherBytes = Convert.FromBase64String(cipherTextBase64);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                using (var ms = new MemoryStream(cipherBytes))
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                using (var sr = new StreamReader(cs))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
     }
 }
