9e3e46e8f84ef3859f525ea79f8719cdb521fadb
pjames <pjames@gmail.com>
2025-08-31 07:37:36 +0700

    Added data generated date & ttl logic

--- onix-api/ModelsViews/MVScanItemResult.cs
+++ onix-api/ModelsViews/MVScanItemResult.cs
@@ -9,6 +9,17 @@ namespace Its.Onix.Api.ModelsViews
         public string? Status { get; set; }
         public string? DescriptionThai { get; set; }
         public string? DescriptionEng { get; set; }
+
         public MScanItem? ScanItem { get; set; }
+
+        /* Object generated date, to determine if this data is too old */
+        public DateTime? DataGeneratedDate { get; set; }
+        public int? TtlMinute { get; set; }
+
+        public MVScanItemResult()
+        {
+            DataGeneratedDate = DateTime.UtcNow;
+            TtlMinute = 5; //5 minutes TTL since DataGeneratedDate
+        }
     }
 }
