3b1ccd7282c7ba1b64c824281cead25262e8f902
pjames <pjames@gmail.com>
2025-09-20 07:04:49 +0700

    Added ThemeVerify

---
+++ onix-api/Migrations/20250919235626_ThemeVerify_001.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace onix.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThemeVerify_001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "theme_verify",
+                table: "ScanItemActions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "theme_verify",
+                table: "ScanItemActions");
+        }
+    }
+}
