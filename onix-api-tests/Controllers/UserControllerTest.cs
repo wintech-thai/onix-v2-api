dd0400df912fb31ed59b317c0895ea3d82ac7df9
pjames <pjames@gmail.com>
2025-09-27 06:12:11 +0700

    Added unit testing

--- onix-api-tests/Controllers/UserControllerTest.cs
+++ onix-api-tests/Controllers/UserControllerTest.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Its.Onix.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Its.Onix.Api.ModelsViews;
+using System.Text.Json;
 
 namespace Its.Onix.Api.Test.Controllers;
 
@@ -117,5 +118,49 @@ public class UserControllerTest
         var t = uc.UpdatePassword("temp", request);
 
         Assert.IsType<OkObjectResult>(t);
+    }
+
+    [Fact]
+    public void AddUserSuccessTest()
+    {
+        var service = new Mock<IUserService>();
+        service.Setup(s => s.AddUser(It.IsAny<string>(), It.IsAny<MUser>())).Returns(new MVUser()
+        {
+            Status = "OK",
+        });
+
+        var uc = new UserController(service.Object);
+
+        var request = new MUser();
+        var t = uc.AddUser("temp", request);
+
+        var result = Assert.IsType<OkObjectResult>(t);
+        var user = Assert.IsType<MVUser>(result.Value);
+        Assert.Equal("OK", user.Status);
+    }
+
+    [Theory]
+    [InlineData("ERROR")]
+    [InlineData("NOTSUCCCESS")]
+    [InlineData("FAIL")]
+    public void AddUserErrorTest(string status)
+    {
+        var service = new Mock<IUserService>();
+        service.Setup(s => s.AddUser(It.IsAny<string>(), It.IsAny<MUser>())).Returns(new MVUser()
+        {
+            Status = status,
+            Description = status,
+        });
+
+        var uc = new UserController(service.Object);
+
+        var request = new MUser();
+        var t = uc.AddUser("temp", request);
+
+        var result = Assert.IsType<BadRequestObjectResult>(t);
+        var errMsg = Assert.IsType<string>(result.Value);
+        Assert.NotNull(errMsg);
+        Assert.NotEmpty(errMsg);
+        Assert.Equal(status, errMsg);
     }
 }
