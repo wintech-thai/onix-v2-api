4d8e951ef2fdad25df11786b3af643c29f506c10
pjames <pjames@gmail.com>
2025-12-03 06:16:49 +0700

    Added Item point redeem field

---
+++ onix-api/Migrations/20251202231509_Item_PointRedeem_001.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace onix.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Item_PointRedeem_001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "point_redeem",
+                table: "Items",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "point_redeem",
+                table: "Items");
+        }
+    }
+}
