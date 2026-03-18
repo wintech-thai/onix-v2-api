35ad0ef67cf82d835ac6e6ef267953a625f65f08
pjames <pjames@gmail.com>
2026-03-18 20:03:05 +0700

    Added GetForgotPasswordLink() API

--- onix-api/examples/User/test-org-user-forgot-password-link.rb
+++ onix-api/examples/User/test-org-user-forgot-password-link.rb
@@ -11,8 +11,8 @@ $stdout.sync = true
 load_env("../.env")
 
 orgId = ENV['API_ORG']
-id = 
-param = '6b293470-411a-4ffa-a404-0f899dfd0090' #'67b1ad7c-8f62-42d4-80e8-2e3de9012785' # 81d68ff0-0d66-440e-91f6-5cf9dd219aa9
+id = 'c0edbdf8-be23-452d-a5f4-27e484e0152e' #'67b1ad7c-8f62-42d4-80e8-2e3de9012785' # 81d68ff0-0d66-440e-91f6-5cf9dd219aa9
+param = nil
 
 ### Inviteuser
 apiUrl = "api/OrganizationUser/org/#{orgId}/action/GetForgotPasswordLink/#{id}"
