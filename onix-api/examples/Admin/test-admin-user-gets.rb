e087d94079e1de2b45730a8152872defc187dcdb
pjames <pjames@gmail.com>
2026-05-11 06:29:13 +0700

    Added InviteUserWithLink() API

---
+++ onix-api/examples/Admin/test-admin-user-gets.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'uri'
+require 'json'
+require './utils'
+
+$stdout.sync = true
+
+################### Main #######################
+load_env("../.env")
+
+orgId = "global"
+keyFile = ".token"
+
+### 
+apiUrl = "admin-api/AdminUser/org/#{orgId}/action/GetUsers"
+param =  {
+  FullTextSearch: "",
+}
+
+token = File.read(keyFile)
+
+ENV['API_KEY'] = nil # ถ้าไม่ใช้ API KEY ก็เซ็ตเป็น nil
+ENV['ACCESS_TOKEN'] = token
+
+#puts("===[#{token}]")
+
+result = make_request(:post, apiUrl, param)
+puts(result)
+
+apiUrl = "admin-api/AdminUser/org/#{orgId}/action/GetUserCount"
+result = make_request(:post, apiUrl, param)
+puts(result)
