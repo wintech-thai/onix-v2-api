0e244d5ea3a4c20eb07f26e855bbc1e37ad8fd43
pjames <pjames@gmail.com>
2026-05-11 11:15:22 +0700

    Added bank account API

---
+++ onix-api/Controllers/AdminUser/AdminBankAccountController.cs
@@ -0,0 +1,96 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Its.Onix.Api.Models;
+using Its.Onix.Api.Services;
+using Its.Onix.Api.ViewsModels;
+
+namespace Its.Onix.Api.Controllers
+{
+    [ApiController]
+    [Authorize(Policy = "GenericRolePolicy")]
+    [Route("/admin-api/[controller]")]
+    public class AdminBankAccountController : ControllerBase
+    {
+        private readonly IBankAccountService svc;
+
+        [ExcludeFromCodeCoverage]
+        public AdminBankAccountController(IBankAccountService service)
+        {
+            svc = service;
+        }
+
+        [ExcludeFromCodeCoverage]
+        [HttpGet]
+        [Route("org/global/action/GetBankAccountById/{bankAccountId}")]
+        public async Task<IActionResult> GetBankAccountById(string bankAccountId)
+        {
+            var result = await svc.GetBankAccountById("global", bankAccountId);
+
+            Response.Headers.Append("CUST_STATUS", result!.Status);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("org/global/action/GetBankAccounts")]
+        public async Task<IActionResult> GetBankAccounts([FromBody] VMBankAccount param)
+        {
+            if (param.Limit <= 0)
+            {
+                param.Limit = 100;
+            }
+
+            var result = await svc.GetBankAccounts("global", param);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("org/global/action/GetAvailableBanks")]
+        public IActionResult GetAvailableBanks()
+        {
+            var result = svc.GetAvailableBanks();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("org/global/action/GetBankAccountCount")]
+        public async Task<IActionResult> GetBankAccountCount([FromBody] VMBankAccount param)
+        {
+            var result = await svc.GetBankAccountCount("global", param);
+            return Ok(result);
+        }
+
+        [ExcludeFromCodeCoverage]
+        [HttpPost]
+        [Route("org/global/action/UpdateBankAccountById/{bankAccountId}")]
+        public async Task<IActionResult> UpdateBankAccountById(string bankAccountId, [FromBody] MBankAccount request)
+        {
+            var result = await svc.UpdateBankAccountById("global", bankAccountId, request);
+
+            Response.Headers.Append("CUST_STATUS", result!.Status);
+            return Ok(result);
+        }
+
+        [ExcludeFromCodeCoverage]
+        [HttpPost]
+        [Route("org/global/action/EnableBankAccountById/{bankAccountId}")]
+        public async Task<IActionResult> EnableBankAccountById(string bankAccountId)
+        {
+            var result = await svc.UpdateBankAccountStatusById(bankAccountId, "Active");
+
+            Response.Headers.Append("CUST_STATUS", result!.Status);
+            return Ok(result);
+        }
+
+        [ExcludeFromCodeCoverage]
+        [HttpPost]
+        [Route("org/global/action/DisableBankAccountById/{bankAccountId}")]
+        public async Task<IActionResult> DisableBankAccountById(string bankAccountId)
+        {
+            var result = await svc.UpdateBankAccountStatusById(bankAccountId, "Disabled");
+
+            Response.Headers.Append("CUST_STATUS", result!.Status);
+            return Ok(result);
+        }
+    }
+}
