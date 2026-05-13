8934768959210f2e7db3ff92f345cbdcdc36d219
pjames <pjames@gmail.com>
2026-05-13 08:54:36 +0700

    Added AdminRoleController

---
+++ onix-api/Controllers/AdminUser/AdminRoleController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Its.Onix.Api.Services;
+using Its.Onix.Api.ViewsModels;
+
+namespace Its.Onix.Api.Controllers
+{
+    [Authorize(Policy = "GenericRolePolicy")]
+    [ApiController]
+    [Route("/admin-api/[controller]")]
+    public class AdminRoleController : ControllerBase
+    {
+        private readonly IRoleService svc;
+
+        [ExcludeFromCodeCoverage]
+        public AdminRoleController(IRoleService service)
+        {
+            svc = service;
+        }
+
+        [ExcludeFromCodeCoverage]
+        // Use POST method, in the future we might send the body
+        [HttpPost]
+        [Route("org/global/action/GetRoles")]
+        public IActionResult GetRoles(string id, [FromBody] VMRole param)
+        {
+            var result = svc.GetRoles(id, param);
+            return Ok(result);
+        }
+    }
+}
