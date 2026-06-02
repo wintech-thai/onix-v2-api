b6c5da18d71ef4c8f104fd9e707f4b70f8b724d4
pjames <pjames@gmail.com>
2026-06-02 15:23:37 +0700

    Added Configuration APIs

---
+++ onix-api/Controllers/AdminUser/AdminConfigurationController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Its.Onix.Api.Services;
+using Its.Onix.Api.Models;
+
+namespace Its.Onix.Api.Controllers
+{
+    [Authorize(Policy = "GenericRolePolicy")]
+    [ApiController]
+    [Route("/admin-api/[controller]")]
+    public class AdminConfigurationController : ControllerBase
+    {
+        private readonly IConfigurationService svc;
+
+        [ExcludeFromCodeCoverage]
+        public AdminConfigurationController(IConfigurationService service)
+        {
+            svc = service;
+        }
+
+        [ExcludeFromCodeCoverage]
+        [HttpGet]
+        [AllowAnonymous] //ยอมให้ทุกคนเข้าถึงได้ เพราะต้องใช้ในการแสดงผลหน้าแรก
+        [Route("org/global/action/GetBrandConfig")]
+        public async Task<IActionResult> GetBrandConfig()
+        {
+            var result = await svc.GetBrandConfig("global");
+            return Ok(result);
+        }
+
+        [ExcludeFromCodeCoverage]
+        [HttpPost]
+        [Route("org/global/action/SetBrandConfig")]
+        public async Task<IActionResult> SetBrandConfig([FromBody] MConfiguration cfg)
+        {
+            var result = await svc.SetBrandConfig("global", cfg);
+            return Ok(result);
+        }
+    }
+}
