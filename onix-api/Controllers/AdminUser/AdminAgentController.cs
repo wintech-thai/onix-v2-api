370e026075875d5fb4a6f6a684da6125f31cedf5
pjames <pjames@gmail.com>
2026-06-16 17:36:51 +0700

    Extract metadata from agent event #4

--- onix-api/Controllers/AdminUser/AdminAgentController.cs
+++ onix-api/Controllers/AdminUser/AdminAgentController.cs
@@ -250,12 +250,16 @@ namespace Its.Onix.Api.Controllers
         [Route("org/global/action/NotifyLineMessage/{agentId}")]
         public async Task<IActionResult> NotifyLineMessage(string agentId, Dictionary<string, object> body)
         {
+            Dictionary<string, object> wrapData = [];
+
             var metaData = string.Join(",", GetMetaData(body));
             var channel = GetChannel(body);
             var pmtLineNoti = GetPaymentNoti(body, channel);
 
-            var eventJson = JsonSerializer.Serialize(body);
-            body.Add("PaymentNoti", pmtLineNoti!);
+            wrapData.Add("InputData", body);
+            wrapData.Add("PaymentNoti", pmtLineNoti!);
+
+            var eventJson = JsonSerializer.Serialize(wrapData);
 
             var evt = new MAgentEvent()
             {
