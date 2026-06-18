4f292a01cfd6c89459946106db55c48d25fbc463
pjames <pjames@gmail.com>
2026-06-19 05:14:13 +0700

    Added PaymentTx discard cent

---
+++ onix-api/Migrations/20260618221236_PaymentTx_DiscardCent_001.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace onix.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentTx_DiscardCent_001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "discard_cent",
+                table: "PaymentTransactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "discard_cent",
+                table: "PaymentTransactions");
+        }
+    }
+}
